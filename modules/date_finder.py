a759806f650dddf69413c8bfa1606ff48078ad4c
EliasRodkey <elias.rodkey@gmail.com>
2021-02-23 12:06:25 -0800

    Finishes date_finder, now contains most desired date ranges

--- modules/date_finder.py
+++ modules/date_finder.py
@@ -25,16 +25,25 @@ class DateFinder():
         # create lists of weeks and a list of months and years to filter by later
         self.now = datetime.datetime.now()
         self.all_time_dates = {
-            "old" : input_start_date, 
-            "new" : input_end_date
+            "start" : input_start_date, 
+            "end" : input_end_date
         }
         self.years = self.find_years(self.all_time_dates)
         self.one_year_today = self.find_year_from_today()
+        self.months = self.find_months(self.all_time_dates)
+        self.one_month_today = self.find_month_from_today(1)
+        self.three_months_today = self.find_month_from_today(3)
+        self.six_months_today = self.find_month_from_today(6)
+        self.weeks = self.find_weeks()
+        self.one_week_toady = self.find_weeks_from_today(1)
+        self.two_week_toady = self.find_weeks_from_today(2)
+        self.three_week_toady = self.find_weeks_from_today(3)
+        self.six_week_toady = self.find_weeks_from_today(6)
 
     # returns the start and end dates of each year in the transactions history
     def find_years(self, all_time_range):
-        start = all_time_range["old"].year
-        end = all_time_range["new"].year
+        start = all_time_range["start"].year
+        end = all_time_range["end"].year
         years_passed = end - start
         years = []
         for i in range(years_passed + 1):
@@ -53,30 +62,46 @@ class DateFinder():
     # finds all of the months with transactions returned in a list of tuples
     def find_months(self, all_time_range):
         months = []
-        current = oldest
-        while current <= newest:
+        current = all_time_range["start"]
+        while current <= all_time_range["end"]:
             last_day = calendar.monthrange(current.year, current.month)[1]
             range_string = f"{current.strftime('%m/01/%Y')} - {current.strftime(f'%m/{last_day}/%Y')}"
             if range_string not in months:
                 months.append(range_string)
             current += datetime.timedelta(days=6)
         months = self.sort_dates(months)
-        return pd.DataFrame(months, columns=["Date Ranges"])
+        return months
     
-    # finds all the weeks (7 day periods) from first transaction until the beginning of transaciton hsitory
+    # finds designated of months going back from today
+    # a months is defined as 30 days for the purposes of this function
+    def find_month_from_today(self, months:int):
+        time_delta = datetime.timedelta(days=30*months)
+        month_ago = self.now - time_delta
+        rang = datetimes_to_range({"start" : month_ago, "end" : self.now})
+        return [rang]
+    
+    # finds all the weeks (7 day periods) from first transaction until the 
+    # beginning of transaciton hsitory
     def find_weeks(self):
         weeks = []
-        for i, month in self.months.iterrows():
-            month = month["Date Ranges"].split(" - ")[0]
+        for month in self.months:
+            month = month.split(" - ")[0]
             month_num = datetime.datetime.strptime(month, "%m/%d/%Y")
             raw_weeks = calendar.Calendar().monthdatescalendar(
                 month_num.year, month_num.month
                 )
             for week in raw_weeks:
-                if f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}" not in weeks:
-                    weeks.append(f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}")
+                rang = f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}"
+                if rang not in weeks:
+                    weeks.append(rang)
         weeks = self.sort_dates(weeks)
-        return pd.DataFrame(weeks, columns=["Date Ranges"])
+        return weeks
+    
+    def find_weeks_from_today(self, weeks:int):
+        time_delta = datetime.timedelta(days=7*weeks)
+        week_ago = self.now - time_delta
+        rang = datetimes_to_range({"start" : week_ago, "end" : self.now})
+        return [rang]
 
     # sorts a range of dates from newest to oldest using quciksort method
     def sort_dates(self, array):
@@ -102,4 +127,5 @@ if __name__ == "__main__":
     start = datetime.date(2019, 3, 26)
     end = datetime.date(2021, 2, 23)
     dates = DateFinder(start, end)
-    print(dates.one_year_today)
+    import pprint
+    pprint.pprint(dates.one_week_toady)
