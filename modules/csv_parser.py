a80b5136d590db0b6235783fc35e1587e74bf562
EliasRodkey <elias.rodkey@gmail.com>
2021-02-23 11:39:45 -0800

    Moves date range finding algos to date_finder

--- modules/csv_parser.py
+++ modules/csv_parser.py
@@ -2,8 +2,6 @@
 # csv_parser.py - reads and sorts data from raw mint csv
 # using pandas dataframe
 import logging
-from tkinter.constants import ALL
-from numpy.lib.shape_base import column_stack
 
 logging.basicConfig(
     level=logging.INFO, 
@@ -12,8 +10,6 @@ logging.basicConfig(
 
 import pandas as pd
 import datetime
-import calendar
-from random import randint
 
 import os
 LOC_TRANSACTION_PATH = os.path.join(os.getcwd(), "transactions.csv")
@@ -102,11 +98,6 @@ def range_to_datetimes(date_range):
     new_date = datetime.datetime.strptime(rang[1], "%m/%d/%Y")
     return {"start" : old_date, "end" : new_date}
 
-def datetimes_to_range(datetimes):
-    old_date = datetimes["start"].strftime("%m/%d/%Y")
-    new_date = datetimes["end"].strftime("%m/%d/%Y")
-    return f"{old_date} - {new_date}"
-
 
 class CSVAnalyzer():
     def __init__(self, local_path, category_breakdown):
@@ -114,17 +105,6 @@ class CSVAnalyzer():
         self.local_path = local_path
         self.df = pd.read_csv(self.local_path)
         self.clean_df()
-
-        # makes sure controller works
-        self.all_time_dates = self.find_all_time_date_range()
-        self.all_time_dates_str = self.all_time_dates["Date Ranges"].iloc[0]
-
-    # returns the date of the first transactions and the date of the last transaction
-    def find_all_time_date_range(self):
-        oldest = self.df["Date"].min()
-        newest = self.df["Date"].max()
-        string = f"{oldest.strftime('%m/%d/%Y')} - {newest.strftime('%m/%d/%Y')}"
-        return pd.DataFrame([string], columns=["Date Ranges"])
         
     def clean_df(self):
         for i, series in self.df.iterrows():
@@ -320,168 +300,3 @@ if __name__ == "__main__":
     )
     print(table.data_frame)
     # analyze.update_csv_file()
-    
-
-
-
-
-class CSVAnalyzer2():
-    def __init__(self, local_path, category_breakdown):
-        # create lists of weeks and a list of months and years to filter by later
-        self.all_time_dates = self.find_all_time_date_range()
-        self.all_time_dates_str = self.all_time_dates["Date Ranges"].iloc[0]
-        self.years = self.find_years(self.all_time_dates_str)
-        self.months = self.find_months(self.all_time_dates_str)
-        self.weeks = self.find_weeks()
-
-    # provides a pandas dataframe with a breakdown of spending / income 
-    # by categpory in a given time frame including a total income, total spending, 
-    # and net gain/loss column
-    def spending_breakdown(self, date_range_df, sub_category=None): 
-        self.categories = {}
-        self.categories["Net Income/Loss"] = []
-        self.categories["Gross Income"] = []
-        self.categories["Gross Spending"] = []
-        if sub_category == None:
-            cat_column = "General Category"
-            for category in self.ALL_CATEGORIES.keys():
-                self.categories[category] = []
-        else:
-            cat_column = "Category"
-            for category in self.ALL_CATEGORIES[sub_category]:
-                self.categories[category] = []
-
-        for i, series in date_range_df.iterrows():
-            rng = series[0].split(" - ")
-            low_date = datetime.datetime.strptime(rng[0], "%m/%d/%Y")
-            high_date = datetime.datetime.strptime(rng[1], "%m/%d/%Y")
-            income_subtotal = 0
-            spending_subtotal = 0
-            for category in list(self.categories.keys())[3:]:
-                filt = (self.df["Date"] >= low_date) \
-                    & (self.df["Date"] <= high_date) \
-                    & (self.df[cat_column] == category);
-                column_sum = self.df[filt]["Amount"].sum()
-                if column_sum > 0:
-                    income_subtotal += column_sum
-                elif column_sum < 0:
-                    spending_subtotal += column_sum
-                else:
-                    self.categories[category].append(0)
-                if column_sum != 0:
-                    self.categories[category].append(column_sum)
-            self.categories["Gross Income"].append(income_subtotal)
-            self.categories["Gross Spending"].append(spending_subtotal)
-            self.categories["Net Income/Loss"].append((income_subtotal + spending_subtotal))
-
-        spending_df = pd.concat(
-            [
-                date_range_df, 
-                pd.DataFrame(self.categories)
-            ], 
-            axis=1
-            )
-        return spending_df.round(2)
-
-    # very similar to the spending breakdown but adds an income
-    # columns and divides all categories by the total to get a percentage
-    # of income in a given period
-    def budget_breakdown(self, date_range_df, sub_category=None, divide_by_income=False):
-        spending_breakdown_df = self.spending_breakdown(date_range_df, sub_category=sub_category)
-        percentages = pd.DataFrame()
-        percentages["Date Ranges"] = date_range_df["Date Ranges"]
-
-        if divide_by_income:
-            net_column = "Gross Income"
-        else:
-            net_column = "Gross Spending"
-        for series in spending_breakdown_df:
-            if series != None and series != "Date Ranges":
-                try:
-                    percentages[series] = spending_breakdown_df[series].div(abs(spending_breakdown_df[net_column]))
-                except ZeroDivisionError:
-                    logging.error(
-                        f"Zero Division Error: couldn't divide: {spending_breakdown_df[series]}  by {spending_breakdown_df[net_column]}"
-                    )
-        return percentages #.rename(columns={"total"}) # return table dates, categories as a percentage of income or categories as a percentage of spending
-
-    # similar to find_all_time_date range as it only returns a single set of dates
-    # however the range can be customized
-    def custom_date_range(self, old_date:str, new_date=datetime.datetime.now().strftime("%m/%d/%Y")):
-        self.custom_dates_str = f"{old_date} - {new_date}"
-        self.custom_dates = pd.DataFrame([self.custom_dates_str], columns=["Date Ranges"])
-        self.years = self.find_years(self.custom_dates_str)
-        self.months = self.find_months(self.custom_dates_str)
-        self.weeks = self.find_weeks()
-
-   
-
-    # returns the start and end dates of each year in the transactions history
-    def find_years(self, date_range):
-        dates = date_range.split(" - ")
-        oldest = datetime.datetime.strptime(dates[0], "%m/%d/%Y")
-        newest = datetime.datetime.strptime(dates[1], "%m/%d/%Y")
-        years_passed = newest.year - oldest.year
-        years = []
-        for i in range(years_passed + 1):
-            current = oldest.year + i
-            years.append(f"01/01/{current} - 12/31/{current}")
-        years = self.sort_dates(years)
-        return pd.DataFrame(years, columns=["Date Ranges"])
-
-    # finds all of the months with transactions returned in a list of tuples
-    def find_months(self, date_range):
-        dates = date_range.split(" - ")
-        oldest = datetime.datetime.strptime(dates[0], "%m/%d/%Y")
-        newest = datetime.datetime.strptime(dates[1], "%m/%d/%Y")
-        all_time_oldest = datetime.datetime.strptime(self.all_time_dates_str.split(" - ")[0], "%m/%d/%Y")
-        all_time_newest = datetime.datetime.strptime(self.all_time_dates_str.split(" - ")[1], "%m/%d/%Y")
-        if oldest < all_time_oldest:
-            oldest = all_time_oldest
-        if newest > all_time_newest:
-            newest = all_time_newest
-        months = []
-        current = oldest
-        while current <= newest:
-            last_day = calendar.monthrange(current.year, current.month)[1]
-            range_string = f"{current.strftime('%m/01/%Y')} - {current.strftime(f'%m/{last_day}/%Y')}"
-            if range_string not in months:
-                months.append(range_string)
-            current += datetime.timedelta(days=6)
-        months = self.sort_dates(months)
-        return pd.DataFrame(months, columns=["Date Ranges"])
-    
-    # finds all the weeks (7 day periods) from first transaction until the beginning of transaciton hsitory
-    def find_weeks(self):
-        weeks = []
-        for i, month in self.months.iterrows():
-            month = month["Date Ranges"].split(" - ")[0]
-            month_num = datetime.datetime.strptime(month, "%m/%d/%Y")
-            raw_weeks = calendar.Calendar().monthdatescalendar(
-                month_num.year, month_num.month
-                )
-            for week in raw_weeks:
-                if f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}" not in weeks:
-                    weeks.append(f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}")
-        weeks = self.sort_dates(weeks)
-        return pd.DataFrame(weeks, columns=["Date Ranges"])
-
-    # sorts a range of dates from newest to oldest using quciksort method
-    def sort_dates(self, array):
-        if len(array) < 2:
-            return array
-
-        low, same, high = [], [], []
-
-        rand_item = array[randint(0, len(array) - 1)]
-        pivot = datetime.datetime.strptime(rand_item.split(" - ")[0], "%m/%d/%Y")
-
-        for item in array:
-            date = datetime.datetime.strptime(item.split(' - ')[0], "%m/%d/%Y")
-            if date < pivot:
-                low.append(item)
-            elif date == pivot:
-                same.append(item)
-            elif date > pivot:
-                high.append(item)
-        return self.sort_dates(low) + same + self.sort_dates(high)
---
+++ modules/date_finder.py
@@ -0,0 +1,105 @@
+#! python3
+# date_finder.py - uses the user input dates to find various
+# useful date ranges such as past week, 2 weeks, month, 3 months
+# 6 months, all date ranges for weeks, months, and years that will
+# be used in the table constructor
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+import calendar
+import datetime
+from random import randint
+
+def datetimes_to_range(datetimes):
+    old_date = datetimes["start"].strftime("%m/%d/%Y")
+    new_date = datetimes["end"].strftime("%m/%d/%Y")
+    return f"{old_date} - {new_date}"
+
+
+class DateFinder():
+    def __init__(self, input_start_date, input_end_date):
+        # create lists of weeks and a list of months and years to filter by later
+        self.now = datetime.datetime.now()
+        self.all_time_dates = {
+            "old" : input_start_date, 
+            "new" : input_end_date
+        }
+        self.years = self.find_years(self.all_time_dates)
+        self.one_year_today = self.find_year_from_today()
+
+    # returns the start and end dates of each year in the transactions history
+    def find_years(self, all_time_range):
+        start = all_time_range["old"].year
+        end = all_time_range["new"].year
+        years_passed = end - start
+        years = []
+        for i in range(years_passed + 1):
+            current = start + i
+            years.append(f"01/01/{current} - 12/31/{current}")
+        years = self.sort_dates(years)
+        return years
+    
+    # returns range of this date a year ago to current date
+    def find_year_from_today(self):
+        year = self.now.year - 1
+        year_ago = datetime.date(year, self.now.month, self.now.day)
+        rang = datetimes_to_range({"start":year_ago, "end" : self.now})
+        return [rang]
+
+    # finds all of the months with transactions returned in a list of tuples
+    def find_months(self, all_time_range):
+        months = []
+        current = oldest
+        while current <= newest:
+            last_day = calendar.monthrange(current.year, current.month)[1]
+            range_string = f"{current.strftime('%m/01/%Y')} - {current.strftime(f'%m/{last_day}/%Y')}"
+            if range_string not in months:
+                months.append(range_string)
+            current += datetime.timedelta(days=6)
+        months = self.sort_dates(months)
+        return pd.DataFrame(months, columns=["Date Ranges"])
+    
+    # finds all the weeks (7 day periods) from first transaction until the beginning of transaciton hsitory
+    def find_weeks(self):
+        weeks = []
+        for i, month in self.months.iterrows():
+            month = month["Date Ranges"].split(" - ")[0]
+            month_num = datetime.datetime.strptime(month, "%m/%d/%Y")
+            raw_weeks = calendar.Calendar().monthdatescalendar(
+                month_num.year, month_num.month
+                )
+            for week in raw_weeks:
+                if f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}" not in weeks:
+                    weeks.append(f"{week[0].strftime('%m/%d/%Y')} - {week[-1].strftime('%m/%d/%Y')}")
+        weeks = self.sort_dates(weeks)
+        return pd.DataFrame(weeks, columns=["Date Ranges"])
+
+    # sorts a range of dates from newest to oldest using quciksort method
+    def sort_dates(self, array):
+        if len(array) < 2:
+            return array
+
+        low, same, high = [], [], []
+
+        rand_item = array[randint(0, len(array) - 1)]
+        pivot = datetime.datetime.strptime(rand_item.split(" - ")[0], "%m/%d/%Y")
+
+        for item in array:
+            date = datetime.datetime.strptime(item.split(' - ')[0], "%m/%d/%Y")
+            if date < pivot:
+                low.append(item)
+            elif date == pivot:
+                same.append(item)
+            elif date > pivot:
+                high.append(item)
+        return self.sort_dates(low) + same + self.sort_dates(high)
+
+if __name__ == "__main__":
+    start = datetime.date(2019, 3, 26)
+    end = datetime.date(2021, 2, 23)
+    dates = DateFinder(start, end)
+    print(dates.one_year_today)
